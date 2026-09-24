37818c1e992f3034f69414f38ccc44992fd158b4
Bjarne Martinsen <eksyphos@gmail.com>
2026-09-24 11:15:54 +0200

    legger til start på Lab4

---
+++ Lab4/Oppvarming/mutationsquiz.py
@@ -0,0 +1,17 @@
+funA_is_destructive = True
+funA_description = 'fjerner verdi fra a'
+
+funB_is_destructive = False
+funB_description = 'legger bare til verdier i to lister og returnerer de'
+
+funC_is_destructive = True
+funC_description = 'endrer verdiene i global variabelen a'
+
+funD_is_destructive = False
+funD_description = 'endrer bare en lokal liste som den returnerer'
+
+funE_is_destructive = True
+funE_description = 'bruker en .pop til å fjerne verdier'
+
+funF_is_destructive = False
+funF_description = 'gjør ingenting'
